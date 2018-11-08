3a7212cf9e19790d4fde778834760ed634818c15
Miren Cava <miren.cava@gmail.com>
2018-11-08 13:05:30 +0100

    nuevo proyecto

---
+++ WebApplication2/Calculos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Calculos
+    {
+
+
+
+        public string esImpar(string valor)
+        {
+            if (Int32.Parse(valor) % 2 == 0)
+            {
+
+                return "even";
+
+            }
+            else
+            {
+                return "odd";
+
+            }
+        }
+    }
+}
+
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace WebApplication1
+{
+
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblTituloApp.Text = "EvenOrOdd Application";
+
+        }
+
+        /*
+          Fuction : isEven
+          -----------------
+          compute if a number is even or odd
+          input params: checkNumber: it is the number to check in about
+          returns: if the number is even return true 
+                   if the number is odd return false
+                   */
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Calculos cal = new Calculos();
+            Label1.Text = cal.esImpar(checkNumberEven.Value);
+
+
+        }
+
+    }
+
+
+}
